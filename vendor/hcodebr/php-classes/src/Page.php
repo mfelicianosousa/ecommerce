2502fadcb0f5d9d2787fd2dca3e152e459513d6c
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-08-11 16:08:46 -0400

    create class page

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Hcode;
+
+Use Rain\Tpl;
+
+class Page {
+
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data" => []
+    ];
+
+    function __construct($opts = array()) {
+        $this->options = array_merge($this->defaults, $opts); //
+
+        $config = array(
+            "tpl_dir" => $_SERVER['DOCUMENT_ROOT'] . "/views/",
+            "cache_dir" => $_SERVER['DOCUMENT_ROOT'] . "/views-cache/",
+            "debug" => false
+        );
+
+
+        Tpl::configure($config);
+        $this->tpl = new Tpl;
+
+        $this->setData($this->options['data']);
+
+        $this->tpl->draw("header");
+    }
+
+    private function setData($data = array()) {
+        foreach ($data as $key => $value) {
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+    public function setTpl($name, $data = array(), $returnHtml = false) {
+        $this->setData($data);
+        return $this->tpl->draw($name, $returnHtml);
+    }
+
+    public function __destruct() {
+        $this->tpl->draw("footer");
+    }
+
+}
