714f66ead1fc44d152ddf8f7cd9f4b9e568f3597
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-08-14 10:34:15 -0400

    users crud

--- index.php
+++ index.php
@@ -1,65 +1,122 @@
-<?php 
+<?php
+
 session_start();
 
-require_once("vendor/autoload.php");
+require_once 'vendor/autoload.php';
 
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Model\User;
+use Hcode\Model\User;
+use Hcode\Page;
+use Hcode\PageAdmin;
+use Slim\Slim;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function () {
+    $page = new Page();
 
-	$page = new Page();
-
-	$page->setTpl("index");
-   
+    $page->setTpl('index');
 });
 
-$app->get('/admin', function() {
+$app->get('/admin', function () {
+    User::verifyLogin(); // Validates if the user is logged in
 
-	
-	User::verifyLogin(); // Validates if the user is logged in
+    $page = new PageAdmin();
 
-	$page = new PageAdmin();
-
-	$page->setTpl("index");
-   
+    $page->setTpl('index');
 });
 
-$app->get('/admin/login', function() {
+$app->get('/admin/login', function () {
+    $page = new PageAdmin([
+        'header' => false,
+        'footer' => false,
+    ]);
 
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false,
-	]);
-
-	$page->setTpl("login");
-   
+    $page->setTpl('login');
 });
 
-$app->post('/admin/login', function() {
+$app->post('/admin/login', function () {
+    User::login($_POST['login'], $_POST['password']);
 
-	User::login($_POST["login"], $_POST["password"]);
-
-	header("Location: /admin");
-	exit;
-   
+    header('Location: /admin');
+    exit;
 });
 
 // Rota para Logout
-$app->get('/admin/logout', function() {
+$app->get('/admin/logout', function () {
+    User::logout();
+    header('Location: /admin/login');
+    exit;
+});
 
-	User::logout();
-	header("Location: /admin/login");
-	exit;
+// rotas para users (listar todos os usuários)
+$app->get('/admin/users', function () {
+    User::verifyLogin();
+
+    $users = User::listAll();
+
+    $page = new PageAdmin();
+    $page->setTpl('users', [
+        'users' => $users,
+    ]);
+});
+
+// rotas para criar usuário
+$app->get('/admin/users/create', function () {
+    User::verifyLogin();
+    $page = new PageAdmin();
+    $page->setTpl('users-create');
+});
+
+$app->get('/admin/users/:iduser/delete', function ($iduser) {
+    User::verifyLogin();
+
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+	header('Location: /admin/users');
+    exit;
+
+});
+
+// rotas para editar o usuário
+$app->get('/admin/users/:iduser', function ($iduser) {
+    User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int) $iduser);
+
+    $page = new PageAdmin();
+    $page->setTpl('users-update', array(
+		'user' => $user->getValues()
+	));
+});
+
+$app->post('/admin/users/create', function () {
+    User::verifyLogin();
+
+    // var_dump($_POST);
+    $user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+    $user->setData($_POST);
+    $user->save();
+	header('Location: /admin/users');
+    exit;
+});
+
+$app->post('/admin/users/:iduser', function ($iduser) {
+    User::verifyLogin();
+
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();
+	header('Location: /admin/users');
+    exit;
 
 });
 
 $app->run();
-
-?>
