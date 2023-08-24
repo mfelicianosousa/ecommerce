73df902f9a781d62b2278c9b090f894854781889
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-08-24 09:37:02 -0400

    Category VS Products

--- site.php
+++ site.php
@@ -1,15 +1,26 @@
 <?php
 
+use Hcode\Model\Category;
 use Hcode\Model\Product;
 use Hcode\Page;
 
 $app->get('/', function () {
-
     $products = Product::listAll();
 
     $page = new Page();
 
     $page->setTpl('index', [
         'products' => Product::checkList($products),
+    ]);
+});
+
+$app->get('/categories/:idcategory', function ($idcategory) {
+    $category = new Category();
+    $category->get((int) $idcategory);
+
+    $page = new Page();
+    $page->setTpl('category', [
+        'category' => $category->getValues(),
+        'products' => Product::checkList( $category->getProducts());
     ]);
 });
